eb2cddc9bdc7619d504183e2b7259d00108f3569
Smug-Bab <myemailgotdestroyed@gmail.com>
2025-11-25 20:25:25 -0600

    .

--- src/main/java/com/smug/veil/ClientProxy.java
+++ src/main/java/com/smug/veil/ClientProxy.java
@@ -1,5 +1,14 @@
 package com.smug.veil;
 
+import com.smug.veil.entity.EntityGorgon;
+import com.smug.veil.model.ModelGorgon;
+import com.smug.veil.render.entity.RenderGorgon;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
 public class ClientProxy extends CommonProxy {
 
+    public void registerRender() {
+        RenderingRegistry.registerEntityRenderingHandler(EntityGorgon.class, new RenderGorgon(new ModelGorgon(), 1));
+    }
 }
