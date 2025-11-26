a45e52e85eddcc420ef86b6d99686c7769049225
Smug-Bab <myemailgotdestroyed@gmail.com>
2025-11-25 23:05:37 -0600

    .

--- src/main/java/com/smug/veil/CommonProxy.java
+++ src/main/java/com/smug/veil/CommonProxy.java
@@ -1,36 +1,33 @@
 package com.smug.veil;
 
-import com.smug.veil.entity.EntityGorgon;
 import net.minecraft.entity.EntityList;
-import cpw.mods.fml.common.registry.EntityRegistry;
-import com.smug.veil.InitRefs;
+
+import com.smug.veil.entity.EntityGorgon;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-	
+import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class CommonProxy {
 
-    
     public static void registerEntity() {
-	    createEntity(EntityGorgon.class, "EntityGorgon", 0xf57e42, 0xd48f6c);
-	}
+        createEntity(EntityGorgon.class, "EntityGorgon", 0xf57e42, 0xd48f6c);
+    }
 
-	public static void createEntity(Class entityClass, String entityName, int solidC, int spotC) {
-	    int randomId = EntityRegistry.findGlobalUniqueEntityId();
+    public static void createEntity(Class entityClass, String entityName, int solidC, int spotC) {
+        int randomId = EntityRegistry.findGlobalUniqueEntityId();
 
-	    EntityRegistry.registerGlobalEntityID(entityClass, entityName, randomId);
-	    EntityRegistry.registerModEntity(entityClass, entityName, randomId, InitRefs.instance, 64, 1, false);
-	    createEgg(randomId, solidC, spotC);
-	}
+        EntityRegistry.registerGlobalEntityID(entityClass, entityName, randomId);
+        EntityRegistry.registerModEntity(entityClass, entityName, randomId, InitRefs.instance, 64, 1, false);
+        createEgg(randomId, solidC, spotC);
+    }
 
-	private static void createEgg(int randomId, int solidC, int spotC) {
-	    EntityList.entityEggs.put(Integer.valueOf(randomId), new EntityList.EntityEggInfo(randomId, solidC, spotC));
-	}
-	
-	
+    private static void createEgg(int randomId, int solidC, int spotC) {
+        EntityList.entityEggs.put(Integer.valueOf(randomId), new EntityList.EntityEggInfo(randomId, solidC, spotC));
+    }
+
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
     // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
@@ -39,8 +36,8 @@ public class CommonProxy {
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
-	registerEntity();
-	    
+        registerEntity();
+
     }
 
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
